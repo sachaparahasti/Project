7f43ca4a1051f93512ec07bdaca6c09e7cce1b1e
diyanenterprises <hirenpatel41010@gmail.com>
2026-01-09 13:23:57 +0530

    change

--- my-app/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.sachaparahasti.loansipapp',
-  appName: 'my-app',
-  webDir: 'build'
-};
-
-export default config;
